0f5bae6077ace73e85ecc6db883f15f742eacb60
Denis G <denis.ga@cmstuning.com>
2025-06-14 21:33:01 +0300

    fix: backend

--- backend.tf
+++ backend.tf
@@ -4,7 +4,6 @@ terraform {
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
-    use_lockfile   = true
     encrypt        = true
     profile        = "aws-rs-devops"
   }
